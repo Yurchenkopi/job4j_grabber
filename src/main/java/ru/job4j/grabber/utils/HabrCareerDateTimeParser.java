cf60f2befca86d7ab6aad8cb07b19243ad6ae2fc
Pavel <heppie@yandex.ru>
2022-08-21 14:05:37 +0300

    2.1. Преобразование даты [#289476 #334740]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -2,16 +2,12 @@ package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        final DateTimeFormatter TIMESTAMP_PARSER = new DateTimeFormatterBuilder()
-                .parseCaseInsensitive()
-                .append(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ"))
-                .toFormatter();
-        return LocalDateTime.parse(parse, TIMESTAMP_PARSER);
+        final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
+        return LocalDateTime.parse(parse, FORMATTER);
     }
 }
